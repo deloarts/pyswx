7fd36bf93ccd6a9c468c795d1bae8c6a2620a89c
Philip Delorenzo <git@deloarts.com>
2025-08-19 09:43:23 +0200

    Update conftest logic

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,14 +7,16 @@ from pathlib import Path
 
 import pytest
 
+from tests.scripts import create_test_drawing_01
 from tests.scripts import create_test_part_01
 from tests.scripts import create_test_part_02
 
 TEST_ROOT = Path(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "tests")
 TEST_OBJECTS = Path(TEST_ROOT, "objects")
-TEST_PART_01 = Path(TEST_OBJECTS, "test_part_01.sldprt")
-TEST_PART_01_EXPORT_STEP = Path(TEST_OBJECTS, "test_part_01.step")
-TEST_PART_02 = Path(TEST_OBJECTS, "test_part_02.sldprt")
+TEST_PART_01 = Path(TEST_OBJECTS, "test_01.sldprt")
+TEST_PART_01_EXPORT_STEP = Path(TEST_OBJECTS, "test_01.step")
+TEST_PART_02 = Path(TEST_OBJECTS, "test_02.sldprt")
+TEST_DRAWING_01 = Path(TEST_OBJECTS, "test_01.slddrw")
 
 
 def pytest_sessionstart():
@@ -36,15 +38,18 @@ def pytest_sessionstart():
         )
     assert doc_count == 0, "\n\n\n\nClose all documents before running tests!\n\n\n"
 
-    # Ensure test object folder
+    # Ensure test object folder has all required files
     os.makedirs(TEST_OBJECTS, exist_ok=True)
-    if os.path.exists(TEST_PART_01):
-        os.remove(TEST_PART_01)
+    if not TEST_PART_01.exists():
+        create_test_part_01.run(file_location=TEST_PART_01)
+    if not TEST_PART_02.exists():
+        create_test_part_02.run(file_location=TEST_PART_02)
+    if not TEST_DRAWING_01.exists():
+        create_test_drawing_01.run(part_file_location=TEST_PART_01, drawing_file_location=TEST_DRAWING_01)
+
+    # Remove generic files
     if os.path.exists(TEST_PART_01_EXPORT_STEP):
         os.remove(TEST_PART_01_EXPORT_STEP)
-
-    create_test_part_01.run(file_location=TEST_PART_01)
-    create_test_part_02.run(file_location=TEST_PART_02)
 
 
 def pytest_sessionfinish():
